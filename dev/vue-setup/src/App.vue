4c81e27c4096ed6cf019a4f2f07b1b9f7b45a7f1
Yasser Fathallah <YasserFat7alah@gmail.com>
2025-04-23 13:58:40 +0200

    Simple to do list

--- dev/vue-setup/src/App.vue
+++ dev/vue-setup/src/App.vue
@@ -1,29 +1,67 @@
 <script setup >
-   import { ref } from 'vue';
+   import { ref, onMounted } from 'vue';
 
       const name = ref('Yasser Fathallah');
       const status = ref('active');
-      const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
+      const tasks = ref([]);
       const link = ref('https://google.com');
+      const newTask = ref('');
 
       const toggleStatus = () =>{
         if(status.value === 'active') status.value = 'pending';
         else if(status.value === 'pending') status.value ='inactive';
         else status.value = 'active';
+      };
+
+      const addTask = () => {
+        if(newTask.value.trim()) {
+          tasks.value.push(newTask.value);
+          newTask.value = '';
+        }
+      };
+
+      const deleteTask = (index) => {
+        tasks.value.splice(index, 1);
       }
-        
+
+      onMounted( async () =>{
+        try {
+            const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+            const data = await res.json();
+            tasks.value = await data.map((task) => task.title);
+        } catch (error) {
+          
+        }
+      })
+
 </script>
 
 <template>
+
+<!-- NAME -->
 <h1>{{ name }}</h1>
 
+<!-- STATUS -->
 <p v-if="status=== 'active' ">User is active</p>
 <p v-else-if="status === 'pending' ">User is pending</p>
 <p v-else>User is inactive</p>
 
+<hr>
+<!-- TASK LIST -->
 <h3>Tasks:</h3>
-<li v-for="task in tasks" :key="task">{{ task }}</li>
+<li v-for="(task, index) in tasks" :key="task">
+  <button @click="deleteTask(index)">x</button>
+  <span>{{ task }}</span>
+</li>
+<br/><br/>
+<!-- ADD A TASK -->
+<form @submit.prevent="addTask">
+  <label for="newTask" > Add a task:  </label>
+  <input type="text" name="newTask" id="newTask" v-model="newTask" />
+  <button type="submit">submit</button>
+</form>
 
+<hr>
 <h3>Links:</h3>
 <!-- <a v-bind:href="link">Google</a> -->
 <a :href="link">Google</a>
