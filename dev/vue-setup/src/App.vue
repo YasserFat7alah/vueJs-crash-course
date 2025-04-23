ea1028a8877142c17d1f8146b4fecf402d6daa03
Yasser Fathallah <YasserFat7alah@gmail.com>
2025-04-23 12:57:04 +0200

    variation using setup function

--- dev/vue-setup/src/App.vue
+++ dev/vue-setup/src/App.vue
@@ -1,43 +1,48 @@
 <script >
+   import { ref } from 'vue';
+
   export default {
-    data() {
+    setup() {
+      const name = ref('Yasser Fathallah');
+      const status = ref('active');
+      const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
+      const link = ref('https://google.com');
+
+      const toggleStatus = () =>{
+        if(status.value === 'active') status.value = 'pending';
+        else if(status.value === 'pending') status.value ='inactive';
+        else status.value = 'active';
+      }
+      
       return{
-        name: 'Yasser fathallah',
-        status: 'pending',
-        tasks: ['task 1', 'task 2', 'task 3'],
-        link: 'https://www.google.com',
-      }; 
-    },
-    methods: {
-      toggleStatus () {
-        if(this.status === 'active') this.status = 'pending';
-        else if(this.status === 'pending') this.status ='inactive';
-        else this.status = 'active';
-      },
-    },
-  };
-</script>
-<!--              -->
-<template>
-  <h1>{{ name }}</h1>
-
-  <p v-if="status=== 'active' ">User is active</p>
-  <p v-else-if="status === 'pending' ">User is pending</p>
-  <p v-else>User is inactive</p>
-
-  <h3>Tasks:</h3>
-  <li v-for="task in tasks" :key="task">{{ task }}</li>
-
-  <h3>Links:</h3>
-  <!-- <a v-bind:href="link">Google</a> -->
-  <a :href="link">Google</a>
-  <br>
-  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
-  <button @click="toggleStatus">Change Status</button>
-</template>
-<!--              -->
-<style scoped>
-  h1 {
-    color: red
+        name,
+        status,
+        tasks,
+        link,
+        toggleStatus
+      }
+    }
   }
+</script>
+
+<template>
+<h1>{{ name }}</h1>
+
+<p v-if="status=== 'active' ">User is active</p>
+<p v-else-if="status === 'pending' ">User is pending</p>
+<p v-else>User is inactive</p>
+
+<h3>Tasks:</h3>
+<li v-for="task in tasks" :key="task">{{ task }}</li>
+
+<h3>Links:</h3>
+<!-- <a v-bind:href="link">Google</a> -->
+<a :href="link">Google</a>
+<br>
+<!-- <button v-on:click="toggleStatus">Change Status</button> -->
+<button @click="toggleStatus">Change Status</button>
+</template>
+
+<style scoped>
+
 </style>
